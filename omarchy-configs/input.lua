4c9930a21355649c20ff83e85ac491fcf2864175
kushal tamang <kushaltamang0926@gmail.com>
2026-08-15 15:20:42 +0545

    added omarchy configs

---
+++ omarchy-configs/input.lua
@@ -0,0 +1,23 @@
+-- mouse and keyboard settings
+hl.config({
+  input = {
+--     -- Start with numlock on by default.
+        numlock_by_default = true,
+--
+--     -- Increase sensitivity for mouse/trackpad (default: 0).
+        sensitivity = 1,
+--
+--
+   touchpad = {
+--       -- Use natural (inverse) scrolling.
+        natural_scroll = true,
+--
+--       -- Use two-finger clicks for right-click instead of lower-right corner.
+        lickfinger_behavior = true,
+--
+--       -- Left-click-and-drag with three fingers.
+        drag_3fg = 1,
+     },
+   },
+ })
+
---
+++ omarchy-configs/looknfeel.lua
@@ -0,0 +1,17 @@
+-- Change the default Omarchy look'n'feel.
+hl.config({
+   general = {
+      border_size = 0,
+   },
+})
+
+hl.config({
+   decoration = {
+      -- Use round window corners.
+      rounding = 2,
+      -- disabling transparent background more then 1 will work
+      active_opacity = 1.5,
+      inactive_opacity = 1.5,
+      fullscreen_opacity = 1.5,
+   },
+})
